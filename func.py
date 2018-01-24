f168c7988a32416aa23d62fa9ee639002fc40248
amnonskalka <35772616+amnonskalka@users.noreply.github.com>
2018-01-24 18:20:25 +0200

    Update func.py

--- func.py
+++ func.py
@@ -1,36 +1,6 @@
 import itertools
 
 
-def ip_options_old(a):
-    tmp_list = []
-    opt = {}
-    ip_list = []
-    opt_num = 0
-
-    for x in a:
-        if x.isdigit():
-            if len(tmp_list) < 3:
-                tmp_list.append(x)
-            else:
-                opt_dic = map(str, tmp_list)
-                opt['opt' + str(opt_num)] = ''.join(opt_dic)
-                tmp_list = []
-                tmp_list.append(x)
-                opt_num += 1
-        elif x == '.':
-            opt_dic = map(str, tmp_list)
-            opt['opt' + str(opt_num)] = ''.join(opt_dic)
-            ip_list.append(opt)
-            tmp_list = []
-            opt = {}
-
-    opt_dic = map(str, tmp_list)
-    opt['opt' + str(opt_num)] = ''.join(opt_dic)
-    ip_list.append(opt)
-
-    return ip_list
-
-
 def store_details(opt, part_cnt, tmp_list):
     key = "part" + str(part_cnt)
     opt_dic = map(str, tmp_list)
